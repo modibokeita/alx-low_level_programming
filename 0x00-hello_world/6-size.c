048674874d49c962a790873d120d878ca3fa6f47
Modibo Keita <modibokeita9390@gmail.com>
2023-08-10 13:56:11 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)", sizeof(char));
+	printf("Size of a int: %d bytes(s)", sizeof(int));
+	printf("Size of a long int: bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)", sizeof(float));
+	return (0);
+}
