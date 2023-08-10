dedd9ea5409abb175d20c406dd860da43981214f
Modibo Keita <modibokeita9390@gmail.com>
2023-08-10 12:58:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+  * main- prints a message
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
