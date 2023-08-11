92565775517eb36cec2ae301c03a27fd852c5f2d
Modibo Keita <modibokeita9390@gmail.com>
2023-08-11 12:13:05 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("Last digit of %d is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n,n);
 	}
 	if (n == 0)
 	{
