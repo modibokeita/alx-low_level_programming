30ea54f841a3d36aed1887f4827d32ac7323d42c
Modibo Keita <modibokeita9390@gmail.com>
2023-08-11 13:38:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - prints all alphabets in lowercase execp e and q
+  * Return: always succes
+  */
+int main(void)
+{
+	char letter[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' || letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+}
