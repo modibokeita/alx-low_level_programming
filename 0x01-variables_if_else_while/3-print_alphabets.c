c7e6e5ab818be4f90f56de651a5bf774909a5ce6
Modibo Keita <modibokeita9390@gmail.com>
2023-08-11 13:09:56 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	char c, i;
 
-	for (c = 'a'; c <= 'z', c++)
+	for (c = 'a'; c <= 'z'; c++);
 	putchar(c);
-	for (i = 'A'; i <= 'Z'; i++)
+	for (i = 'A'; i <= 'Z'; i++);
 	putchar(i);
 	putchar('\n');
 	return (0);
