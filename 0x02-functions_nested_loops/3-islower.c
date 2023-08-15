ff4beb2b7ee84062d7c05e20459b573c4f8ae644
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 11:16:17 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _islower - check if c is lowercase
+  * Return: 1 otherwise 0
+  */
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
