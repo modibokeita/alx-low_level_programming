9c2c2780641b0aba9f8ae01b8b015e61b1b255c9
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 13:24:19 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
   * _islower - check if c is lowercase
-  *@parameter c takes aphabets
+  *@c: checks aphabets
   * Return: 1 otherwise 0
   */
 int _islower(int c)
 {
 
-	if (c >= 98 && c <= 102)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
