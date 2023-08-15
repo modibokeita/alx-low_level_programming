6206cc11d3b1cb010292df31ea9e2b4a7eed89cf
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 13:09:12 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,11 @@
 int _islower(int c)
 {
 
-	if (c == 'a' || c == 'b' || c == 'd' || c == 'e' || c == 'f' || c == 'g' || c == 'h' || 'i' || 'j' || c == 'k' || c == 'l' || c == 'm' || c == 'n' || c == 'o' || c == 'p' || c == 'q' || c == 'r' || c == 's' || c == 't' || c == 'u' || c == 'v' || c == 'w' || c == 'x' || c == 'y' || c == 'z')
+	if (c == 'a' || c == 'z')
 	{
 		return (1);
 	}
-	else if ( c == 'C')
-	{
-		return (0);
-	}
-	else
+	else 
 	{
 		return (0);
 	}
