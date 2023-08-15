6681fb919b915e07b14ee9123619cdfef197d0c5
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 08:59:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+  * main - prints alphabet
+  * Return: always 0
+  */
+int main(void)
+{
+	alphabet('a');
+	alphabet('b');
+	alphabet('c');
+	alphabet('d');
+	alphabet('e');
+	alphabet('f');
+	alphabet('g');
+	alphabet('h');
+	alphabet('i');
+	alphabet('j');
+	alphabet('k');
+	alphabet('l');
+	alphabet('m');
+	alphabet('n');
+	alphabet('o');
+	alphabet('p');
+	alphabet('q');
+	alphabet('r');
+	alphabet('s');
+	alphabet('t');
+	alphabet('u');
+	alphabet('v');
+	alphabet('w');
+	alphabet('x');
+	alphabet('y');
+	alphabet('z');
+	return (0);
+}
