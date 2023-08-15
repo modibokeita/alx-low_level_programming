70594de8934033515dc2a058a77bf7b36901e0e9
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 09:26:07 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,31 +6,6 @@
   */
 int main(void)
 {
-	/*print_alphabet('a');*/
-	print_alphabet('b');
-	print_alphabet('c');
-	print_alphabet('d');
-	print_alphabet('e');
-	print_alphabet('f');
-	print_alphabet('g');
-	print_alphabet('h');
-	print_alphabet('i');
-	print_alphabet('j');
-	print_alphabet('k');
-	print_alphabet('l');
-	print_alphabet('m');
-	print_alphabet('n');
-	print_alphabet('o');
-	print_alphabet('p');
-	print_alphabet('q');
-	print_alphabet('r');
-	print_alphabet('s');
-	print_alphabet('t');
-	print_alphabet('u');
-	print_alphabet('v');
-	print_alphabet('w');
-	print_alphabet('x');
-	print_alphabet('y');
-	print_alphabet('z');
+	print_alphabet();
 	return (0);
 }
