5bfc208b6ebe2eaf34bdcd81d243df6acd8572dd
Modibo Keita <modibokeita9390@gmail.com>
2023-08-15 13:32:35 -0700

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * @c: checks the alphabet
+  * Return: 1 otherwise 0
+  */
+int _isalpha(int c)
+{
+	if (c <= 97 || c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
