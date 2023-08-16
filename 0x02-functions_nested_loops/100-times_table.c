09d5c1b8e7513e927540a973caba9b11f26b0ef9
Modibo Keita <modibokeita9390@gmail.com>
2023-08-16 10:59:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,18 +7,42 @@
   */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 4; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 4; j <= i; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if (i != 15 && i < 0)
+			for (j = 0; j <= n; j++)
 			{
-				printf("%d", i);
-				printf("\n");
-				printf("%d", j);
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
 	}
 }
