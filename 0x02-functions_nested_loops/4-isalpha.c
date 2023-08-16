0983f1384947bd0c81415bef97c7c74b6e1fab0b
Modibo Keita <modibokeita9390@gmail.com>
2023-08-16 06:54:12 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,12 @@
   */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 98) || (c <= 97 && c <= 122))
+	if (c >= 65 && c <= 90)
 	{
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
 		return (1);
 	}
 	else
