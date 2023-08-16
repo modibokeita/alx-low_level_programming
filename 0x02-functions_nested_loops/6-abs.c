f076c5cc91ec86d5714419a8da7d43e3a5d6cb06
Modibo Keita <modibokeita9390@gmail.com>
2023-08-16 08:23:56 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _abs - computes the absolute value of an integer
+  * @i: the integer to be checked
+  * Return: absolute value
+  */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
+
