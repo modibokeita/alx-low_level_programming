17503ec5fc576ba66cd9fbebc945cdd9dd4488b9
Modibo Keita <modibokeita9390@gmail.com>
2023-08-16 09:05:28 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-  * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+  * jack_bauer - prints every minute of the day of Jack Bauer
   * Return: always 0
   */
 void jack_bauer(void)
 {
 	int i, j;
+
 	i = 0;
 
 	while (i < 24)
