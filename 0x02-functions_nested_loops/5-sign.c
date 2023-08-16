f1c6f5bb309dd656f2d935e69f431d0e56948004
Modibo Keita <modibokeita9390@gmail.com>
2023-08-16 07:59:26 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
   * print_sign - checks the sign of numbers
   * @n: the numbers to be checked
-  * Retrun: 1 and prints + if n is greater than zero or 0 and prints 0 if n is zero or -1 and prints - if n is less than zero
+  * Return: 1 and prints + if n is greater than zero or 0 and prints 0 if n is zero or -1 and prints - if n is less than zero
   */
 int print_sign(int n)
 {
@@ -10,15 +10,6 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	}else if (n == 0)
+	
 }
