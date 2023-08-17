607280e85e767063a0436755589d2ac39192ec3c
Modibo Keita <modibokeita9390@gmail.com>
2023-08-17 08:19:32 -0700

    0-main.c, main.h file

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -11,5 +12,6 @@ int main(void)
 	i = 98;
 
 	positive_or_negative(i);
+	
 	return (0);
 }
