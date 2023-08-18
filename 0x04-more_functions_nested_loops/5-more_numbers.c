610a5ea513835bb7878204a37fac623ee3a534f2
Modibo Keita <modibokeita9390@gmail.com>
2023-08-18 07:44:34 -0700

    prints numbers 10 times from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * more_numbers - prints 10 times from 0 to 14
+  * Return: always 0
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+	}
+	_putchar(10);
+}
