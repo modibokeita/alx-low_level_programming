d67a5187c2ad2b28c7f3da3cb2137848b53ba295
Modibo Keita <modibokeita9390@gmail.com>
2023-08-21 13:12:33 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * puts_half -  prints half of a string
+  * @str: the string to be print
+  * Return: 0
+  */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	j = i / 2;
+
+	if (i % 2 == 1)
+		j++;
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
