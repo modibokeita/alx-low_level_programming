31553cd6b0791113fa1612420e9d3ae80188b656
Modibo Keita <modibokeita9390@gmail.com>
2023-08-21 09:52:25 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  * _strlen - returns the length of a string
+  * @s: the length of string to be return
+  * Return: the length of a string
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
