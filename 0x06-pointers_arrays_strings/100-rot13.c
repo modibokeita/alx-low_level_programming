278edf67df0578fcb82ddceb56436da62ab65f80
Modibo Keita <modibokeita9390@gmail.com>
2023-08-24 10:39:38 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string using rot13.
+  * @s: string to be encoded
+  * Return: the string enconded
+  */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == str[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
