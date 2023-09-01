b09114fefaa70e90e735e9b5e1f581f2860dae7e
Modibo Keita <modibokeita9390@gmail.com>
2023-09-01 15:25:35 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: the size of argv
+  * @argv: argument vector
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
