b06eebf34e3151ce1c913232fb33b299068ba69f
Modibo Keita <modibokeita9390@gmail.com>
2023-09-01 14:55:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - prints its name
+  * @argc: the size of array argv
+  * @argv: the array contains the size of argc
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
