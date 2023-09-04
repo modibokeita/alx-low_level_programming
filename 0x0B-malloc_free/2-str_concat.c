d2b4042ec0493d40fa55a6a878916fb720e15dfb
Modibo Keita <modibokeita9390@gmail.com>
2023-09-04 14:00:48 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,30 @@
   */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-
+	int i, len1 = strlen(s1), len2 = strlen(s2);
 	char *s;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
-
 	s = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len1; i++)
 	{
 		s[i] = s1[i];
