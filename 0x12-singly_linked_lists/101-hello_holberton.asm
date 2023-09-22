7a3d204006705da9c0628b93f8698308efb7a68c
Modibo Keita <modibokeita9390@gmail.com>
2023-09-22 08:17:54 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	hello db "Hello, Holberton",0
+	nl db 10,0
+
+section .text
+	global main
+
+	extern printf
+main:
+	push rdi
+	lea rdi, [rel hello]
+	call printf
+
+	lea rdi, [rel nl]
+	call printf
+
+	pop rdi
+	
+
+	ret
