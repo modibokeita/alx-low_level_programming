c1f3562d5edbec108a6916e44ab6a255d4946e7e
Modibo Keita <modibokeita9390@gmail.com>
2023-09-28 12:29:44 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * get_endianness -  checks the endianness
+  * Return: 0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	unsigned char *byte_ptr = (unsigned char *)&num;
+
+	return ((int)byte_ptr[0]);
+}
