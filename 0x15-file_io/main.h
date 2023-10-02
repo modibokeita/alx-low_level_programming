1d9568b03a5fa2b827bd006e28a28a029374603c
Modibo Keita <modibokeita9390@gmail.com>
2023-10-02 13:24:57 -0700

    some changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,12 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void handle_error(int exit_code, const char *message);
+int main(int argc, char *argv[]);
 #endif
