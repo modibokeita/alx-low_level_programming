03d63bff922f85eff11cc714c87a9b2e0119fa7f
Modibo Keita <modibokeita9390@gmail.com>
2023-10-02 11:12:39 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * create_file -  creates a file
+  * @filename: name of fille
+  * @text_content: the content of file
+  * Return: 1 on success, -1 on failure
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t b_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		b_written = write(fd, text_content, strlen(text_content));
+
+		if (b_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
