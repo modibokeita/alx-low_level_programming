09a14611e4b7ff8712ec5027c5a8f1c5993d5192
Modibo Keita <modibokeita9390@gmail.com>
2023-10-26 10:19:41 -0700

    header Files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
